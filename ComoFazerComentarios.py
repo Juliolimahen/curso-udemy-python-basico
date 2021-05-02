ed9a0556b3736ff25c04ed6429f06f43c6b1cd89
Juliolimahen <juliolima_henrique@outlook.com>
2021-05-02 17:04:11 -0300

    second commit

---
+++ ComoFazerComentarios.py
@@ -0,0 +1,16 @@
+#adicionar acentos
+#-*- coding: utf-8 -*-
+
+print("Olá mundo!")
+print("Outra mensagem!")
+
+# isto é um comentário 
+# outo comentario 
+print("nova linha")
+
+""" Comentário 
+de linhas 
+multiplas 
+"""
+
+
