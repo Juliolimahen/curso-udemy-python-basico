8a634618592e094edd1d4e4f0f80b9881fbb25de
unknown <juliolima_henrique@outlook.com>
2021-05-03 08:55:09 -0300

    funcoes

---
+++ Funcoes.py
@@ -0,0 +1,25 @@
+#palvra reservada def
+#sintaxe def NOME (PARAMETROS)
+    #COMANDOS
+#chamada NOME (ARGUMENTOS)  
+#funcao soma
+def soma(x, y):
+    return x+y
+def subtracao(x, y):
+    return x-y
+
+def multiplicacao(x, y):
+    return x*y
+def divisao(x, y):
+    return x/y
+
+#preciso armazenar em uma variavel  
+s=soma(2,3)
+
+m=multiplicacao(2,3)
+
+d=divisao(2,3)
+
+sb=subtracao(2,3)
+
+print("soma:",s ,"\nsubtração:",sb ,"\nmultiplicação:",m ,"\ndivisao:",d)
